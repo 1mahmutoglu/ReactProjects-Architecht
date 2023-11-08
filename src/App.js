ace3459161749ffde67ee2623227f2b530c46f84
1mahmutoglu <mrfrkmhmtgl@gmail.com>
2023-11-08 10:36:18 +0300

    CountExample
    1 adet Count componenti oluşturuldu
    2 adet buton oluşturuldu
    Oluşturulan bir adet butona arttırma fonksiyonu yazıldı, bootstrap kullanılarak buton stilli yapıldı ve "on" adında fonkiyon ile arttırma 10 sayısında durduruldu.
    
    Oluşturulan bir adet butona azaltma fonksiyonu yazıldı, bootstrap kullanılarak buton stilli yapıldı ve "sifir" adında fonkiyon ile azaltma 0 sayısında durduruldu.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Count from './components/count/Count'
+
+function App() {
+  return (
+<div className="">
+<div className="container">
+  <div className="row">
+    <div className="col-4">
+      <Count/>
+    </div>
+    <div className="col-4">
+      BBBBB
+    </div>
+    <div className="col-4">
+      CCCC
+    </div>
+  </div>
+</div>
+</div>
+  );
+}
+
+export default App;
