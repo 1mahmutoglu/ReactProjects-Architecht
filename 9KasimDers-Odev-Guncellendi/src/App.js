cfba308a697d36e6613d415067270c39d77ef455
1mahmutoglu <mrfrkmhmtgl@gmail.com>
2023-11-12 21:27:23 +0300

    9Kasim-Odev-Guncellendi
    State hook'u kullanılarak oluşturulan kişinin isim, soyisim, yaş, email ve yorum bilgilerini içeren yazıları ekrana yazacak şekilde ekleme, düzenleme ve silme fonksiyonları tam çalışır halde düzenlenmiştir.

---
+++ 9KasimDers-Odev-Guncellendi/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import KisiEkle from './KisiEkle';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <KisiEkle/>
+        </div>
+ 
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
